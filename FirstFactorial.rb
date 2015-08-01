3e8d4025de892b4dc709588891989c25fa3b37f4
bensies13 <bensies13@gmail.com>
2015-08-01 15:25:36 +0900

    Create FirstFactorial.rb

---
+++ FirstFactorial.rb
@@ -0,0 +1,19 @@
+def FirstFactorial(num)
+
+  erer=1
+  while num > 1
+    erer= erer*num
+    num=num-1
+  end
+  
+  
+  
+  # code goes here
+  return erer
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+FirstFactorial(STDIN.gets)           
+
